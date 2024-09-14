f3d2f8ee1dd1187d6cfe6f6440e3ccdd3a7d5a5b
Paulo Roberto <paulonill@gmail.com>
2024-09-14 09:31:56 -0300

    Finish UserService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -37,6 +37,51 @@ class UserService {
             throw error;
         }
     }
+
+    async getById(id: string) {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id
+                }
+            });
+            return user;
+        } catch (error) {
+            console.error(`Error fetching user. ${error}`);
+            throw error;
+        }
+    }
+
+    async delete(id: string){
+        try {
+            await prisma.user.delete({
+                where: {
+                    id
+                }
+            });
+        } catch (error) {
+            console.error(`Error deleting user. ${error}`);
+            throw error;
+        }
+    }
+
+    async update(id: string, name: string, email: string, password: string) {
+        try {
+            const hashPassword = await hash(password, 10);
+            const user = await prisma.user.update({
+                where: {id},
+                data: {
+                    name,
+                    email,
+                    password: hashPassword
+                }
+            });
+            return user;
+        } catch (error) {
+            console.error(`Error updating user. ${error}`);
+            throw error;
+        }
+    }
 }
 
 export { UserService }
